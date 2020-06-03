846bf368b173bd13f68c3a5592d9fb9cd1cf2da9
Marko Putnik <putnikmarko689@gmail.com>
2020-06-03 18:34:21 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,89 @@
+#define OLC_PGE_APPLICATION
+#include <iostream>
+#include "olcPixelGameEngine_2.0.h"
+
+using namespace std;
+using namespace olc;
+
+class Pong : public PixelGameEngine {
+
+public:
+
+	Pong() {
+		sAppName = "PONG GAME";
+	}
+
+private:
+
+	Sprite *wallSprite = nullptr;
+	Decal *wallDecOne = nullptr;
+	Decal *wallDecTwo = nullptr;
+
+	Sprite *ballSprite = nullptr;
+	Decal *ballDec = nullptr;
+
+	int m_nScoreOne, m_nScoreTwo;
+
+	float fDecOneY, fDecOneX;
+	float fDecTwoY, fDecTwoX;
+
+public:
+
+	bool OnUserCreate() override{
+
+		m_nScoreOne = 0;
+		m_nScoreTwo = 0;
+
+		wallSprite = new Sprite("assets/wall.png");
+		wallDecOne = new Decal(wallSprite);
+		wallDecTwo = new Decal(wallSprite);
+
+		ballSprite = new Sprite("assets/ball.png");
+		ballDec = new Decal(ballSprite);
+
+		fDecOneX = -1.0f;
+		fDecTwoX = ScreenWidth() - 10.0f;
+
+		fDecOneY = ScreenHeight() / 3.0f;
+		fDecTwoY = ScreenHeight() / 3.0f;
+
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override{
+
+
+		// Clearthe screen
+		Clear(DARK_BLUE);
+
+		// Calculating input for player one
+		if (GetKey(Key::UP).bHeld && fDecTwoY > 0.0f) {
+
+			fDecTwoY -= 0.02f;
+
+		}
+		else if (GetKey(Key::DOWN).bHeld && fDecTwoY < ScreenHeight() - 35.0f) {
+
+			fDecTwoY += 0.02f;
+
+		}
+
+		// Drawing our decals
+		DrawDecal({ fDecOneX, fDecOneY }, wallDecOne, { 0.1f, 0.1f });
+		DrawDecal({ fDecTwoX, fDecTwoY }, wallDecTwo, { 0.1f, 0.1f });
+
+		return true;
+	}
+
+};
+
+int main() {
+
+	Pong pong;
+	if (pong.Construct(160, 120, 5, 5)) {
+		pong.Start();
+	}
+
+	system("pause");
+	return 0;
+}
